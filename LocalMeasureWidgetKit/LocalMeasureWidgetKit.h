5c816abb7b1fd7d4c7e92246f575890bf3fd9a69
solal <solal.fitoussi@gmail.com>
2016-04-25 14:16:11 -0400

    Initial Commit

---
+++ LocalMeasureWidgetKit/LocalMeasureWidgetKit.h
@@ -0,0 +1,19 @@
+//
+//  LocalMeasureWidgetKit.h
+//  LocalMeasureWidgetKit
+//
+//  Created by solal on 25/04/2016.
+//  Copyright © 2016 solal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalMeasureWidgetKit.
+FOUNDATION_EXPORT double LocalMeasureWidgetKitVersionNumber;
+
+//! Project version string for LocalMeasureWidgetKit.
+FOUNDATION_EXPORT const unsigned char LocalMeasureWidgetKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalMeasureWidgetKit/PublicHeader.h>
+
+
